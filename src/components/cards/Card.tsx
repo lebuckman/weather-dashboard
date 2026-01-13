817d4b08419e33d876d1e1821c5c0665ec0da737
lebuckman <liambuckman0@gmail.com>
2026-01-13 11:49:29 -0800

    build(style): set up shadcn/ui

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -4,7 +4,7 @@ type Props = { children: ReactNode; title: string; className?: string };
 
 export default function Card({ children, title, className }: Props) {
     return (
-        <div className="flex flex-col gap-4 p-4 rounded-xl bg-zinc-900 shadow-md">
+        <div className="flex flex-col gap-4 p-4 rounded-xl bg-linear-to-br from-card to-card/40 shadow-md">
             <h2 className="text-2xl font-semibold">{title}</h2>
             <div className={className}>{children}</div>
         </div>
